20c1db13a85164184f819fb51ba0554c4c1c7acc
SergeyKirintsev <kserg80@gmail.com>
2021-12-07 07:11:09 +0300

    feat: apollo/client

--- game-store/src/pages/catalog/Catalog.jsx
+++ game-store/src/pages/catalog/Catalog.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
+import { useQuery, gql } from "@apollo/client";
 
 import Card from '../../components/card/Card';
 import './Catalog.css'
 
 const CatalogScreen = () => {
-  const cards = [];
+  let cards = [];
+
+  const query = gql`
+    query ExampleQuery {
+        locations {
+            results {
+                id
+                name
+                residents {
+                    id
+                    name
+                    status
+                    species
+                    type
+                    gender
+                    image
+                }
+            }
+        }
+    }
+`;
+
+  const { loading, error, data } = useQuery(query);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  cards = data.locations.results[0].residents;
 
   return (
     <>
       <div className="Catalog">
         {cards.map((card) => (
-          <Card props={card} />
+          <Card props={card}/>
         ))}
       </div>
     </>
